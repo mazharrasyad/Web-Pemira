53afe345eaea3353212efedd4c68079cdf713600
mazharrasyad <muhazharrasyad@gmail.com>
2020-01-08 07:33:33 +0700

    mulai

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <title>Pemira</title>
+    </head>
+    <body>
+        <br><br><br>
+        <center>
+            <img src="image/logo.png" height="150px">            
+        </center>
+
+        <br><br>
+        <form action="" method="post">
+            <center>
+                <table>
+                    <tr>
+                        <td colspan="2"><label><b>Verifikasi Identitas</b></label></td>
+                    </tr>                
+                    <tr>
+                        <td><hr></td>
+                    </tr>
+                    <tr>
+                        <td>NIM</td>
+                        <td><input type="text"></td>                        
+                    </tr>
+                    <tr>
+                        <td>Sandi</td>
+                        <td><input type="text"></td>                        
+                    </tr>
+                    <tr colspan="2">
+                        <td>
+                            <input type="submit" value="Cek">
+                        </td>
+                    </tr>
+                </table>
+            </center>
+        </form>
+    </body>
+</html>
